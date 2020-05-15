89b29b31da7da4a3acd9e6fbeebdf11cdfe3afa6
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 21:50:33 +0700

    Create Jobsheet 9 C++.cpp
    Program Perbandingan

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int nim=18;
+	int tgl=1;
+	int nilaiterkecil;
+	nilaiterkecil=(nim<tgl)? tgl:nim;
+	cout<<"jadi hasil nilai terkecil antara"<<nim<<"dan"<<tgl<<"adalah"<<nilaiterkecil;
+}
