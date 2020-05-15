82cee075cbd8e206f3b5fc48e718234f643e944f
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 21:56:04 +0700

    Create Studi kasus C.c
    Buatlah program untuk menampilkan output dari  Nama dan NIM seluruh anggota kelompok

---
+++ Studi kasus C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	printf("BIODATA KELOMPOK 17\n");
+	printf("Nama ketua Kelompok = lalu Tohirul Ahyar\n");
+	printf("NIM                 = F1B019081\n");
+	printf("Nama Anggota        = lalu reza adrian\n");
+	printf("NIM                 = F1B019080\n");
+	printf("Nama Anggota        = M.Ramdanil Abidin\n");
+	printf("NIM                 = F1B019082\n");
+	printf("Nama Anggota        =M Abdul azis\n");
+	printf("NIM                 = F1B019083\n");
+	printf("Nama Anggota        =Nur Afifah Syarifah\n");
+	printf("NIM                 = F1B019108\n");
+}
